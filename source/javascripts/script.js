9a1850ab8e18527650bf551344409a186af02811
Nicole Zhu <nzhu94@gmail.com>
2016-02-07 11:01:12 -0600

    edit chatroom styles a bit

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  // $('#show-forum').on('click', function() {
-  //   $('#show-forum').hide();
-  //   $('.game-forum-container').show();
-  // });
+  $('#show-forum').on('click', function() {
+    $('#show-forum').hide();
+    $('.game-forum-container').show();
+  });
 
   $('#chat-box').on('keypress', function(e) {
     if (e.keyCode == 13) {
       e.preventDefault();
       var message = $('#chat-box').val();
-      $('.clearfix').before('<div class="chat new"><div class="chat-info"><span class="chat-info">@tacolover <span class="chat-time">2/6/16, 15:43 pm</span></span></div><span class="chat-message">' + message + '</span></div>');
+      $('.clearfix').before('<div class="chat new"><div class="chat-info"><span class="chat-info">@mrrobot <span class="chat-time">2/6/16, now pm</span></span></div><span class="chat-message">' + message + '</span></div>');
       $('#chat-box').val('');
     }
   });
